8fda523e05c4d19a14002b959c7a32608ea74cd1
Baopvg1009 <114211379+Baopvg1009@users.noreply.github.com>
2023-07-07 20:44:05 +0700

    Movie 1

---
+++ screens/MovieScreen.js
@@ -0,0 +1,111 @@
+import {
+  View,
+  Text,
+  ScrollView,
+  TouchableOpacity,
+  Platform,
+  Dimensions,
+  SafeAreaView,
+  Image,
+} from "react-native";
+import React, { useEffect, useState } from "react";
+import { useNavigation, useRoute } from "@react-navigation/native";
+import { ArrowLeftIcon, ChevronLeftIcon } from "react-native-heroicons/outline";
+import { HeartIcon } from "react-native-heroicons/solid";
+import { styles, theme } from "../theme";
+import { LinearGradient } from "expo-linear-gradient";
+import Cast1 from "../components/cast";
+var { width, height } = Dimensions.get("window");
+const ios = Platform.OS == "ios";
+const topMargin = ios ? "" : "mt-1";
+
+export default function MovieScreen() {
+  const { params: item } = useRoute();
+  const navigation = useNavigation();
+  const [isFavourite, toggleFavourite] = useState(false);
+  const [cast, setCast] = useState([1, 2, 3, 4, 5]);
+  let movieName = "Ant-Man and the wasp: Qualumiana";
+
+  useEffect(() => {
+    //call the api
+  }, [item]);
+  return (
+    <ScrollView
+      contentContainerStyle={{ paddingBottom: 40 }}
+      className="flex-1 bg-neutral-900 pt-7"
+    >
+      {/* back button and movie poster */}
+      <View className="w-full">
+        <SafeAreaView
+          className={
+            "absolute z-20 w-full flex-row justify-between items-center px-4  " +
+            topMargin
+          }
+        >
+          <TouchableOpacity
+            style={styles.background}
+            className="rounded-xl p-1"
+            onPress={() => navigation.goBack()}
+          >
+            <ChevronLeftIcon
+              size="28"
+              style={styles.background}
+              strokeWidth={2.5}
+              color="white"
+            />
+          </TouchableOpacity>
+          <TouchableOpacity onPress={() => toggleFavourite(!isFavourite)}>
+            <HeartIcon
+              size="35"
+              color={isFavourite ? theme.background : "white"}
+            />
+          </TouchableOpacity>
+        </SafeAreaView>
+        <View>
+          <Image
+            source={require("../assets/images/moviePoster2.png")}
+            style={{ width, height: height * 0.55 }}
+          />
+          <LinearGradient
+            colors={["transparent", "rgba(23,23,23,0.8)", "rgba(23,23,23,1)"]}
+            style={{ width, height: height * 0.4 }}
+            start={{ x: 0.5, y: 0 }}
+            end={{ x: 0.5, y: 1 }}
+            className="absolute bottom-0"
+          />
+        </View>
+      </View>
+
+      {/* Movie detail */}
+      <View style={{ marginTop: -(height * 0.09) }} className="space-y-3">
+        {/* Title */}
+        <Text className="text-white text-center text-3xl font-bold tracking-wider">
+          {movieName}
+        </Text>
+        {/* status, release, runtime */}
+        <Text className="text-neutral-400 font-semibold text-base text-center">
+          Released • 2023 • 170 min
+        </Text>
+        <View className="flex-row justify-center mx-4 space-x-2">
+          <Text className="text-neutral-400 font-semibold text-base text-center">
+            Action •
+          </Text>
+          <Text className="text-neutral-400 font-semibold text-base text-center">
+            Thrill •
+          </Text>
+          <Text className="text-neutral-400 font-semibold text-base text-center">
+            Comedy •
+          </Text>
+        </View>
+        {/* Destcription */}
+        <Text className="text-neutral-400 mx-4 tracking-wide">
+          Scott Lang and Hope Van Dyne are dragged into the Quantum Realm, along
+          with Hope's parents and Scott's daughter Cassie. Together they must
+          find a way to escape, but what secrets is Hope's mother hiding? And
+          who is the mysterious Kang?{" "}
+        </Text>
+      </View>
+      <Cast1 cast={cast} />
+    </ScrollView>
+  );
+}
